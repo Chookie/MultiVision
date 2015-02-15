82306f27ca6b0981c6e0f0eadc3b660cffe53fe8
Alison <alison@johnston.name>
2015-02-15 21:45:22 +0000

    Trying to get mvNotify to work

--- public/app/common/mvNotifier.js
+++ public/app/common/mvNotifier.js
@@ -1,16 +1,13 @@
 
 // Handle notifications
-angular.module('app').value('toastr', toastr);
 
-angular.module('app').factory('mvNotifier', ['toastr', function(toastr){
-    // THIS CODE IS KIND OF STUPID. wraps toastr library in a service
-    // globally accessible anyhow
+angular.module('app').factory('mvNotifier', function(){
     return {
         notifySuccess: function(message){
-            toastr.success(message)
+            window.toastr.success(message)
         },
         notifyFail: function(message){
-            toastr.error(message)
+            window.toastr.error(message)
         }
     }
 }]);
