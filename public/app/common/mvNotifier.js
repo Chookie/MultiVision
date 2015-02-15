626b79b8f7f0d3dbb06a98fdfba195501b37f147
Alison <alison@johnston.name>
2015-02-15 21:33:07 +0000

    Trying to get mvNotify to work

--- public/app/common/mvNotifier.js
+++ public/app/common/mvNotifier.js
@@ -1,22 +1,10 @@
-(function (ngapp) {
+angular.module('app').value('mvToastr', toastr);
 
-    // Wrap toastr global variable in service so can use in our dependency injection and use that to inject it below
-    ngapp.value('mvToastr', window.toastr);
-
-
-    // Service
-    ngapp.factory('mvNotifier',[ 'mvToastr', function (mvToastr) {
-        //mvToastr.options = {"timeOut": "2000", "closeButton": true };
-        return{
-            notifySuccess: function (msg) {
-                mvToastr.success(msg);
-                console.log(msg);
-            },
-            notifyFail: function (msg) {
-                mvToastr.error(msg);
-                console.log(msg);
-            }
-        };
-    }]);
-
-}(window.angular.module('app')));
+angular.module('app').factory('mvNotifier', function(mvToastr) {
+    return {
+        notify: function(msg) {
+            mvToastr.success(msg);
+            console.log(msg);
+        }
+    }
+})
